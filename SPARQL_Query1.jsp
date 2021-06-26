413518224578718ed2cea72765b2a1b8f24f2942
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-06-26 16:03:37 +0700

    Update 26/06/2564 16.03

--- SPARQL_Query1.jsp
+++ SPARQL_Query1.jsp
@@ -1,17 +1,23 @@
 <form action="index.jsp" method="POST">
-    <textarea name="qstring" value="${param.qstring}" rows="10" cols="70 ">
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX ex: <http://www.myontology.com/rice#>
-
-      SELECT ?RiceEngName ?RiceThaiName ?Image
-        WHERE { 
-          ?ThaiRice ex:isRiceEngName ?RiceEngName .
-          ?ThaiRice ex:isRiceThaiName ?RiceThaiName .
-          ?ThaiRice ex:hasImage ?Image .
-          ?ThaiRice ex:beType ?tr .
-          ?tr ex:isRiceTypeThaiName ?RiceType .
-        }
-    </textarea>
-  <br><br>
- <input type="submit">
+    <input name="qstring" required value="${param.qstring}" rows="10" cols="70">
+    <br><br>
+    <input type="submit">
 </form>
+
+<%--<form action="index.jsp" method="POST">--%>
+<%--    <textarea name="qstring" value="${param.qstring}" rows="10" cols="70 ">--%>
+<%--      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>--%>
+<%--      PREFIX ex: <http://www.myontology.com/rice#>--%>
+
+<%--      SELECT ?RiceEngName ?RiceThaiName ?Image--%>
+<%--        WHERE { --%>
+<%--          ?ThaiRice ex:isRiceEngName ?RiceEngName .--%>
+<%--          ?ThaiRice ex:isRiceThaiName ?RiceThaiName .--%>
+<%--          ?ThaiRice ex:hasImage ?Image .--%>
+<%--          ?ThaiRice ex:beType ?tr .--%>
+<%--          ?tr ex:isRiceTypeThaiName ?RiceType .--%>
+<%--        }--%>
+<%--    </textarea>--%>
+<%--  <br><br>--%>
+<%-- <input type="submit">--%>
+<%--</form>--%>
--- WEB-INF/classes/com/example/mySparql.java
+++ WEB-INF/classes/com/example/mySparql.java
@@ -22,6 +22,18 @@ import com.hp.hpl.jena.util.FileManager;
 public class mySparql extends TagSupport {
 
 	private String qstring = "";
+	private String queryTemplat = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+			"      PREFIX ex: <http://www.myontology.com/rice#>\n" +
+			"\n" +
+			"      SELECT ?RiceEngName ?RiceThaiName ?Image\n" +
+			"        WHERE { \n" +
+			"          ?ThaiRice ex:isRiceEngName ?RiceEngName .\n" +
+			"          ?ThaiRice ex:isRiceThaiName ?RiceThaiName .\n" +
+			"          ?ThaiRice ex:hasImage ?Image .\n" +
+			"          ?ThaiRice ex:beType ?tr .\n" +
+			"          ?tr ex:isRiceTypeThaiName ?RiceType .\n" +
+			"        \n" +
+			"        }";
 
 	public void setQstring(String qstring) {
 		this.qstring = qstring;
@@ -32,6 +44,21 @@ public class mySparql extends TagSupport {
 		JspWriter out = pageContext.getOut();
 		try {
 			if (!qstring.equals("")) {
+				queryTemplat = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+						"      PREFIX ex: <http://www.myontology.com/rice#>\n" +
+						"\n" +
+						"      SELECT ?RiceEngName ?RiceThaiName ?Image\n" +
+						"        WHERE { \n" +
+						"          ?ThaiRice ex:isRiceEngName ?RiceEngName .\n" +
+						"          ?ThaiRice ex:isRiceThaiName ?RiceThaiName .\n" +
+						"          ?ThaiRice ex:hasImage ?Image .\n" +
+						"          ?ThaiRice ex:beType ?tr .\n" +
+						"          ?tr ex:isRiceTypeThaiName ?RiceType .\n" +
+						"        \n" +
+						"          FILTER regex(?RiceEngName, " + qstring + ", \"i\")\n" +
+						"        }";
+
+
 				FileManager.get().addLocatorClassLoader(mySparql.class.getClassLoader());
 				Model model = FileManager.get().loadModel("thricerdf.owl");
 				Query query = QueryFactory.create(qstring);
@@ -53,7 +80,7 @@ public class mySparql extends TagSupport {
 					out.print("<th>" + list.get(i) + "</th>");
 				}
 				out.print("</tr>");
-				
+
 				while (results.hasNext()) {
 					out.print("<tr>");
 					QuerySolution soln = results.nextSolution();
@@ -71,3 +98,77 @@ public class mySparql extends TagSupport {
 		return SKIP_BODY;
 	}
 }
+
+//package com.example;
+//
+//import java.util.Calendar;
+//import java.util.Iterator;
+//import java.util.List;
+//
+//import javax.servlet.jsp.JspException;
+//import javax.servlet.jsp.JspWriter;
+//import javax.servlet.jsp.tagext.TagSupport;
+//
+//import com.hp.hpl.jena.query.Query;
+//import com.hp.hpl.jena.query.QueryExecution;
+//import com.hp.hpl.jena.query.QueryExecutionFactory;
+//import com.hp.hpl.jena.query.QueryFactory;
+//import com.hp.hpl.jena.query.QuerySolution;
+//import com.hp.hpl.jena.query.ResultSet;
+//import com.hp.hpl.jena.query.ResultSetFormatter;
+//import com.hp.hpl.jena.rdf.model.Literal;
+//import com.hp.hpl.jena.rdf.model.Model;
+//import com.hp.hpl.jena.util.FileManager;
+//
+//public class mySparql extends TagSupport {
+//
+//	private String qstring = "";
+//
+//	public void setQstring(String qstring) {
+//		this.qstring = qstring;
+//	}
+//
+//	public int doStartTag() throws JspException {
+//
+//		JspWriter out = pageContext.getOut();
+//		try {
+//			if (!qstring.equals("")) {
+//				FileManager.get().addLocatorClassLoader(mySparql.class.getClassLoader());
+//				Model model = FileManager.get().loadModel("thricerdf.owl");
+//				Query query = QueryFactory.create(qstring);
+//				QueryExecution qexec = QueryExecutionFactory.create(query, model);
+//
+//				ResultSet results = qexec.execSelect();
+//				if (results.getRowNumber() != 0) {
+//					out.print(results.getResultVars() + "<br> This is a result: <br>");
+//				}
+//
+//				// out.println(ResultSetFormatter.asText(results));
+//
+//				out.print("<br> This is a result: <br>");
+//				List<String> list = results.getResultVars();
+//				out.print("---------------------------<br>");
+//				out.print("<table>");
+//				out.print("<tr>");
+//				for (int i = 0; i < list.size(); i++){
+//					out.print("<th>" + list.get(i) + "</th>");
+//				}
+//				out.print("</tr>");
+//
+//				while (results.hasNext()) {
+//					out.print("<tr>");
+//					QuerySolution soln = results.nextSolution();
+//					for (int i = 0; i < list.size(); i++){
+//						Literal var1 = soln.getLiteral(list.get(i));
+//						out.print("<td>" + var1 + "</td>");
+//					}
+//					out.print("</tr>");
+//				}
+//				out.print("</table>");
+//			}
+//		} catch (Exception e) {
+//			System.out.println(e);
+//		}
+//		return SKIP_BODY;
+//	}
+//}
